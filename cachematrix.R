b2bb4a0928aec943a889fa1a7c50daa85d71ddf2
Cristian Zantedeschi <c.zantedeschi@gmail.com>
2017-02-01 10:15:10 +0000

    cacheMatrix third commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,11 @@
 # Peer-graded Assignment: Programming Assignment 2: Lexical Scoping 
 
 
+
+# 
+
 # makeCacheMatrix: 
 #         This function creates a special "matrix" object that can cache its inverse.
-# cacheSolve: 
-#         This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-#         If the inverse has already been calculated (and the matrix has not changed), 
-#         then the cachesolve should retrieve the inverse from the cache.
-# 
-# Computing the inverse of a square matrix can be done with the solve function in R. 
-# For example, if X is a square invertible matrix, then solve(X) returns its inverse.
-
 
 # makeCacheMatrix creates a list containing a function to
 # 1. set the value of the matrix
@@ -34,7 +29,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# cacheSolve: 
+#         This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#         If the inverse has already been calculated (and the matrix has not changed), 
+#         then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
